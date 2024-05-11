baba4a30934cba2c115cdafb62f2a3ee89a6cdd3
Antonio Neto <antoniogfneto92@hotmail.com>
2024-05-11 14:41:37 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
+        Seller obj = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000., new Department());
         System.out.println(obj);
     }
 }
