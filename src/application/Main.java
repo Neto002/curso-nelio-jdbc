71bd796327ace07b84c8c463fef9be5d5c19971d
Antonio Neto <antoniogfneto92@hotmail.com>
2024-05-11 14:48:34 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,5 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Seller obj = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000., new Department());
         System.out.println(obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
     }
 }
